7e0527724a88155fb6b9c120f0c0881f09150403
Shokhzod2001 <shahzodabdumalikov1@gmail.com>
2025-11-16 20:22:07 +0900

    feat: Integrating Routing and Navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,9 @@
 import { Text, View } from "react-native";
-import "./global.css";
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-blue-600">
-      <Text className="text-white text-3xl font-bold">
+      <Text className="text-dark-200 text-3xl font-bold">
         Tailwind is Working!
       </Text>
     </View>
